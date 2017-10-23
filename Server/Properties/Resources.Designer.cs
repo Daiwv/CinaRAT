135a98278f5d1bac3b99b8dc75c227f5ab09a432
Wearelegal <contact@wearelegal.es>
2017-10-23 18:46:59 +0200

    .
    .

--- Server/Properties/Resources.Designer.cs
+++ Server/Properties/Resources.Designer.cs
@@ -441,17 +441,17 @@ namespace xServer.Properties {
         }
         
         /// <summary>
-        ///   Busca una cadena traducida similar a     Quasar - Remote Administration Tool
-        ///    Copyright (C) 2016 MaxX0r
+        ///   Busca una cadena traducida similar a     Yggdrasil - Remote Administration Tool
+        ///    Copyright (C) 2017 Wearelegal
         ///
-        ///    Quasar is free software: you can redistribute it and/or modify
+        ///    Yggdrasil is free software: you can redistribute it and/or modify
         ///    it under the terms of the GNU General Public License as published by
         ///    the Free Software Foundation, either version 3 of the License, or
         ///    (at your option) any later version.
         ///
-        ///    Quasar is distributed in the hope that it will be useful,
+        ///    Yggdrasil is distributed in the hope that it will be useful,
         ///    but WITHOUT ANY WARRANTY; without even the implied warranty of
-        ///    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See [resto de la cadena truncado]&quot;;.
+        ///    MERCHANTABILITY or FITNESS FOR A PARTICULAR  [resto de la cadena truncado]&quot;;.
         /// </summary>
         internal static string TermsOfUse {
             get {
